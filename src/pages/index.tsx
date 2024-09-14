aad0f749564101f36e18c7711b8f830e4ccd2664
SkillGG <skilletssgames@gmail.com>
2024-09-15 01:41:08 +0200

    Landing page 0.1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,15 @@ export default function Home() {
         <meta name="description" content="An app to learn kanjis!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Link href={"/list"}>Kanji progress tracking list</Link>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <div className="text-2xl">Kanji learning app hub</div>
+        <Link
+          href={"/list"}
+          className="mt-2 cursor-pointer rounded-xl border-2 border-slate-400 bg-slate-600 p-2 text-[lime] underline hover:bg-slate-500"
+        >
+          Kanji progress tracking list
+        </Link>
+      </div>
     </>
   );
 }
